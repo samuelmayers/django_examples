ced90cc45c4c6ab6a22123d7461ec769d0b0e4cf
samuel_mayers <samuel.mayers22@hotmail.com>
2022-11-07 23:36:11 +0000

    Changes to be committed: 	modified:   polls/templates/polls/index.html 	modified:   polls/urls.py 	modified:   polls/views.py

--- polls/views.py
+++ polls/views.py
@@ -7,16 +7,14 @@ from .models import Question
 
 def index(request):
     latest_quesyion_list = Question.objects.all()
-    return render(request, "polls/index.html", {
-        "latest_quesyion_list": latest_quesyion_list
-    })
+    context = {'latest_question_list': latest_question_list}
+    return render(request, "polls/index.html", context)
 
 
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {
-        "question": question
-    })
+    context = {"question": question}
+    return render(request, "polls/detail.html", context)
 
 
 def results(request, question_id):
