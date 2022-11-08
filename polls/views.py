5fb7b1fb41c685a303ab21d48a3362ee0e3486ac
samuel_mayers <samuel.mayers22@hotmail.com>
2022-11-08 00:36:05 +0000

    Changes to be committed: 	new file:   polls/templates/polls/results.html 	modified:   polls/views.py

--- polls/views.py
+++ polls/views.py
@@ -17,7 +17,9 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    return HttpResponse(f"Estás viendo los resultados de la pregunta número {question_id}")
+    question = get_object_or_404( Question, pk=question_id)
+    context = {"question": question}
+    return render(request, "polls/results.html", context)
 
 
 def vote(request, question_id):
