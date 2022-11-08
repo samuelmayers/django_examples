0a477979f3fe0bc37562fc0d06105e8c7e34df47
samuel_mayers <samuel.mayers22@hotmail.com>
2022-11-08 00:50:02 +0000

    modified:   db.sqlite3 modified:   polls/templates/polls/results.html modified:   polls/views.py

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from django.http import HttpResponse, HttpResponseRedirect
-from .models import Question
+from .models import Question, Choice
 
 
 def index(request):
@@ -17,7 +17,7 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    question = get_object_or_404( Question, pk=question_id)
+    question = get_object_or_404(Question, pk=question_id)
     context = {"question": question}
     return render(request, "polls/results.html", context)
 
@@ -25,7 +25,8 @@ def results(request, question_id):
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
-        selected_choice = Question.choice_set.get(pk=request.POST["choice"])
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+
     except (KeyError, Choice.DoesNotExist):
         context = {
             "question": question,
@@ -33,6 +34,6 @@ def vote(request, question_id):
         }
         return render(request, "polls/detail.html", context)
     else:
-        selected_choice.vote += 1
+        selected_choice.votes += 1
         selected_choice.save()
         return HttpResponseRedirect(reverse("polls:results", args=(question_id,)))
