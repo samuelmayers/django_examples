2a40377a44f9c94a8877829cbbc49e680ab2e981
samuel_mayers <samuel.mayers22@hotmail.com>
2022-11-08 01:12:27 +0000

    modified:   polls/urls.py 	modified:   polls/views.py

--- polls/views.py
+++ polls/views.py
@@ -1,26 +1,23 @@
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from django.http import HttpResponse, HttpResponseRedirect
+from django.views import generic
 from .models import Question, Choice
 
 
-def index(request):
-    latest_question_list = Question.objects.all()
-    context = {'latest_question_list': latest_question_list}
-    return render(request, "polls/index.html", context)
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+    def get_queryset(self):
+        return Question.objects.order_by("pub_date")[:5]    
 
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    context = {"question": question}
-    return render(request, "polls/detail.html", context)
-
-
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    context = {"question": question}
-    return render(request, "polls/results.html", context)
-
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
