1238dfee04288c2daf6f3ee7093ba5c9472fe84b
samuel_mayers <samuel.mayers22@hotmail.com>
2022-11-28 01:02:29 +0000

    new file:   polls/static/polls/detail.css

--- polls/admin.py
+++ polls/admin.py
@@ -1,4 +1,18 @@
 from django.contrib import admin
 from .models import Question, Choice
 
-admin.site.register([Question, Choice])
+class Choiceinline(admin.StackedInline):
+    model= Choice
+    extra = 3
+
+class QuetionAdmin(admin.ModelAdmin):
+    fields = ["question_text"]
+    inlines = [Choiceinline]
+    list_display= ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields=['question_text']
+
+
+admin.site.register(Question,QuetionAdmin)
+
+
